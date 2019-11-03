d68817f47fefbe922a1f77808270c8b3018d245d
Sandra Roques <roquesandra@hotmail.fr>
2019-11-03 20:14:22 +0100

    challenge done

---
+++ src/main/java/com/wildcodeschool/ChallengeSpringAPISchool/ChallengeSpringApiSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.ChallengeSpringAPISchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSpringApiSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSpringApiSchoolApplication.class, args);
+	}
+
+}
